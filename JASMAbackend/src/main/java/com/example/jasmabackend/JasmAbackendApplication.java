7ae30d77aff253e04fa94881b9c90bf90a804960
Doru Cheresdi <doru.cheresdi@yahoo.com>
2023-04-11 22:52:52 +0300

    Added spring project

---
+++ JASMAbackend/src/main/java/com/example/jasmabackend/JasmAbackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.jasmabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JasmAbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JasmAbackendApplication.class, args);
+	}
+
+}
---
+++ JASMAbackend/src/test/java/com/example/jasmabackend/JasmAbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jasmabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JasmAbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
