3d96eb6ee55079ceed9b8fde59c4227c2eeea065
Doru Cheresdi <51168560+DoruCheresdi@users.noreply.github.com>
2023-04-25 18:00:42 +0300

    Added a edit profile button

--- JASMAbackend/src/app/post-list/post-list.component.ts
+++ JASMAbackend/src/app/post-list/post-list.component.ts
@@ -3,6 +3,7 @@ import {Post} from "../post";
 import {UserService} from "../services/user-service.service";
 import {HttpClient, HttpParams} from "@angular/common/http";
 import {AuthenticateService} from "../services/authenticate.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-post-list',
@@ -12,11 +13,18 @@ import {AuthenticateService} from "../services/authenticate.service";
 export class PostListComponent {
 
   posts: Post[];
-  constructor(private http: HttpClient, private auth: AuthenticateService) {
+  constructor(private http: HttpClient,
+              private router: Router,
+              private auth: AuthenticateService) {
     this.posts = [];
   }
 
   ngOnInit() {
+
+    // if email is null:
+    if (this.auth.email == undefined || this.auth.email == "") {
+      this.router.navigateByUrl("/login");
+    }
 
     const params = new HttpParams()
       .set('userEmail', this.auth.email);
