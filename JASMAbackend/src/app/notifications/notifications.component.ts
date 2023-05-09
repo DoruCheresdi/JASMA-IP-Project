d8e3363be0e1371058997965b067d3b9ef79473b
adinaamzarescu <73987572+adinaamzarescu@users.noreply.github.com>
2023-05-09 17:18:20 +0300

    Notifications with backend
    I'm not sure if it works, we have to test it

--- JASMAbackend/src/app/notifications/notifications.component.ts
+++ JASMAbackend/src/app/notifications/notifications.component.ts
@@ -1,50 +1,52 @@
+// Importing necessary modules
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
-interface Notification {
-    id: number;
-    fromUser: string;
-    message: string;
-    timeStamp: Date;
+// Creating a User class with required properties
+class User {
+    constructor(public id: number, public email: string, public message: string, public timeStamp: Date) {}
 }
 
+// Defining the component and its properties
 @Component({
     selector: 'app-notifications',
     templateUrl: './notifications.component.html',
     styleUrls: ['./notifications.component.css'],
 })
 export class NotificationsComponent implements OnInit {
-    notifications: Notification[] = [
-        {
-            id: 1,
-            fromUser: 'John',
-            message: 'Sent you a friend request',
-            timeStamp: new Date(),
-        },
-        {
-            id: 2,
-            fromUser: 'Sarah',
-            message: 'Sent you a friend request',
-            timeStamp: new Date(),
-        },
-        {
-            id: 3,
-            fromUser: 'Tom',
-            message: 'Sent you a friend request',
-            timeStamp: new Date(),
-        },
-    ];
+    friendRequests: User[] = []; // Initializing friendRequests array with User type
 
-    constructor() {}
+    // Injecting HttpClient module in the component constructor
+    constructor(private http: HttpClient) {}
 
-    ngOnInit(): void {}
-
-    acceptFriendRequest(notification: Notification) {
-        // Handle accepting friend request here
-        console.log(`Accepted friend request from ${notification.fromUser}`);
+    // OnInit lifecycle hook
+    ngOnInit(): void {
+        this.getFriendRequests(); // calling getFriendRequests function
     }
 
-    rejectFriendRequest(notification: Notification) {
-        // Handle rejecting friend request here
-        console.log(`Rejected friend request from ${notification.fromUser}`);
+    // Function to get friend requests from the server
+    getFriendRequests() {
+        // Calling HTTP GET method to fetch friend requests from server
+        this.http.get<User[]>('/devapi/friendRequests').subscribe(users => {
+            this.friendRequests = users; // Setting fetched users to friendRequests array
+        });
+    }
+
+    // Function to accept a friend request
+    acceptFriendRequest(user: User) {
+        // Calling HTTP POST method to accept a friend request
+        this.http.post<void>(`/devapi/friendRequest/accept`, user.email).subscribe(() => {
+            console.log(`Accepted friend request from ${user.email}`); // Logging message to console
+            this.getFriendRequests(); // calling getFriendRequests function after successful response
+        });
+    }
+
+    // Function to reject a friend request
+    rejectFriendRequest(user: User) {
+        // Calling HTTP POST method to reject a friend request
+        this.http.post<void>(`/devapi/friendRequest/reject`, user.email).subscribe(() => {
+            console.log(`Rejected friend request from ${user.email}`); // Logging message to console
+            this.getFriendRequests(); // calling getFriendRequests function after successful response
+        });
     }
 }
