c75413e25a165402b2075648b5515549a83ef1ab
adinaamzarescu <73987572+adinaamzarescu@users.noreply.github.com>
2023-05-09 23:52:42 +0300

    Minor Changes
    Some changes to comments and profile

--- JASMAbackend/src/app/entities/feed-comment.ts
+++ JASMAbackend/src/app/entities/feed-comment.ts
@@ -1,11 +1,13 @@
 export class FeedComment {
+
+    authorEmail: string;
+    text: string;
+    sinceCreatedString: string;
     constructor(authorEmail: string, text: string, sinceCreatedString: string) {
         this.authorEmail = authorEmail;
         this.text = text;
         this.sinceCreatedString = sinceCreatedString;
     }
 
-    authorEmail: string;
-    text: string;
-    sinceCreatedString: string;
+
 }
