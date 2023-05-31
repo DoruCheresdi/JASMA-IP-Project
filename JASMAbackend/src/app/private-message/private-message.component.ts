2dceec2fdacee483e5b723645c8518adbae2f72c
Doru Cheresdi <51168560+DoruCheresdi@users.noreply.github.com>
2023-05-31 03:47:46 +0300

    implemented messages

--- JASMAbackend/src/app/private-message/private-message.component.ts
+++ JASMAbackend/src/app/private-message/private-message.component.ts
@@ -1,27 +1,84 @@
-import {Component, OnInit} from '@angular/core';
-import {UserDTO} from "../entities/user-dto";
-import {UserService} from "../services/user-service.service";
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {HttpClient, HttpParams} from "@angular/common/http";
-import {User} from "../entities/user";
-import {Post} from "../entities/post";
 import {AuthenticateService} from "../services/authenticate.service";
+import {ActivatedRoute} from "@angular/router";
+import {UserMessageDTO} from "../entities/user-message-dto";
+import {interval, switchMap, takeWhile} from "rxjs";
 
 @Component({
     selector: 'app-private-message',
     templateUrl: './private-message.component.html',
     styleUrls: ['./private-message.component.css']
 })
-export class PrivateMessageComponent {
+export class PrivateMessageComponent implements OnDestroy, OnInit{
 
-    userDTOs: UserDTO[] = [];
+    userEmail : string = "";
 
-    userName: string = "";
+    messages : UserMessageDTO[] = [];
 
-    constructor(private http: HttpClient, private auth: AuthenticateService) {
+    newMessage = "";
+
+    intervalId : any | undefined;
+
+    constructor(private http: HttpClient, public auth: AuthenticateService, private route: ActivatedRoute) {
     }
 
     ngOnInit() {
+        this.route.queryParams.subscribe(params => {
+                console.log(params["email"]);
+                this.userEmail = params["email"];
+                // this.getMessages();
 
+
+            // @ts-ignore
+            // this.intervalId = setInterval(this.getMessages, 1000);
+            setInterval(()=> { this.getMessages() }, 3 * 1000);
+                // interval(500)
+                //     .pipe(takeWhile(() => !stop))
+                //     .subscribe(() => {
+                //         // place you code here
+                //         this.getMessages();
+                //     });
+            }
+        );
+        // this.heroes$ = this.route.paramMap.pipe(
+        //     switchMap(params => {
+        //         this.selectedId = Number(params.get('id'));
+        //         return this.service.getHeroes();
+        //     })
+        // );
     }
 
+    ngOnDestroy() {
+        clearInterval(this.intervalId);
+    }
+
+    getMessages() {
+
+        const params = new HttpParams()
+            .set('user1Email', this.userEmail)
+            .set('user2Email', this.auth.email);
+
+        // Get user details
+        this.http.get<UserMessageDTO[]>("/devapi/messages", { params: params }).subscribe(
+            (userMessageDTOS: UserMessageDTO[]) => {
+                this.messages = userMessageDTOS;
+            }
+        );
+
+
+        // set timer to call this function again:
+    }
+
+    sendMessage() {
+
+        this.http.post("/devapi/message", {
+            receiverEmail : this.userEmail,
+            content : this.newMessage
+        }).subscribe(
+            () => {
+                this.getMessages();
+            }
+        )
+    }
 }
